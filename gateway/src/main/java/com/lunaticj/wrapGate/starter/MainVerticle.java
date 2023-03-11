46f44bba9efe99369b2dc9f2ad247c8defc9edef
jason <ljc19940130@gmail.com>
2023-03-12 01:26:53 +0800

    reactor code with fileSystem

--- gateway/src/main/java/com/lunaticj/wrapGate/starter/MainVerticle.java
+++ gateway/src/main/java/com/lunaticj/wrapGate/starter/MainVerticle.java
@@ -2,16 +2,15 @@ package com.lunaticj.wrapGate.starter;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
-  public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
+  public void start(Promise<Void> startPromise) {
+    vertx.createHttpServer().requestHandler(req -> req.response()
+      .putHeader("content-type", "text/plain")
+      .end("Hello from Vert.x!")).listen(8888, http -> {
       if (http.succeeded()) {
         startPromise.complete();
         System.out.println("HTTP server started on port 8888");
@@ -19,5 +18,9 @@ public class MainVerticle extends AbstractVerticle {
         startPromise.fail(http.cause());
       }
     });
+  }
+
+  public static void main(String[] args) {
+    Vertx.vertx().deployVerticle(new MainVerticle());
   }
 }
---
+++ gateway/src/main/java/com/lunaticj/wrapGate/starter/ReactorTestWithFileSystem.java
@@ -0,0 +1,49 @@
+package com.lunaticj.wrapGate.starter;
+
+import io.vertx.core.*;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.file.FileSystem;
+import io.vertx.core.file.OpenOptions;
+
+public class ReactorTestWithFileSystem extends AbstractVerticle {
+  public static void main(String[] args) {
+    Vertx vertx = Vertx.vertx();
+    vertx.deployVerticle(new ReactorTestWithFileSystem());
+  }
+
+  @Override
+  public void start(Promise<Void> startPromise) {
+    FileSystem fileSystem = vertx.fileSystem();
+    Future<Void> f1 = fileSystem.createFile("text1.txt")
+      .compose(v -> {
+        System.out.println("f1 point 1");
+        return fileSystem.open("text1.txt", new OpenOptions().setWrite(true));
+      })
+      .compose(asyncFile -> {
+          System.out.println("f1 point 2");
+          return asyncFile.write(Buffer.buffer("write test1"))
+            .onSuccess(v -> {
+              System.out.println("f1 point 3");
+              asyncFile.read(Buffer.buffer(), 0, 0, 16)
+                .onSuccess(buffer -> {
+                  System.out.println("f1 point 4");
+                  System.out.println(buffer);
+                });
+            });
+        }
+      );
+    Future<Void> f2 = fileSystem.createFile("text2.txt").onComplete(v -> System.out.println("f2 complete"));
+    CompositeFuture f3 = CompositeFuture.all(f1, f2);
+    f3.onSuccess(cf -> System.out.println("f3 success")).onFailure(throwable -> {
+      if (f1.failed()) {
+        f1.cause().printStackTrace();
+      }
+      if (f2.failed()) {
+        f2.cause().printStackTrace();
+      }
+    });
+    System.out.println("on the end");
+
+
+  }
+}
