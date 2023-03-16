d6d06cae6115acaa50c79c60149f5449ba3143a3
jason <ljc19940130@gmail.com>
2023-03-16 23:14:20 +0800

    webSocket test 1

---
+++ learn/src/main/java/com/lunaticj/wrapGate/learn/WebSocketServerVerticle.java
@@ -0,0 +1,35 @@
+package com.lunaticj.wrapGate.learn;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.Router;
+
+import java.time.LocalDateTime;
+
+public class WebSocketServerVerticle extends AbstractVerticle {
+  public static void main(String[] args) {
+    Vertx.vertx().deployVerticle(new WebSocketServerVerticle());
+  }
+
+  @Override
+  public void start(Promise<Void> startPromise) {
+    Router router = Router.router(vertx);
+    router.route().handler(rc -> rc.request().toWebSocket()
+      .onSuccess(serverWebSocket -> {
+        serverWebSocket.textMessageHandler(System.out::println);
+        serverWebSocket.writeTextMessage("hello web socket!");
+        String textHandlerID = serverWebSocket.textHandlerID();
+        final long periodicId = vertx.setPeriodic(1000L, l -> vertx.eventBus().send(textHandlerID, LocalDateTime.now().toString()));
+        serverWebSocket.closeHandler(v-> vertx.cancelTimer(periodicId));
+        serverWebSocket.exceptionHandler(Throwable::printStackTrace);
+      })
+      .onFailure(Throwable::printStackTrace));
+
+
+
+    vertx.createHttpServer().requestHandler(router).listen(9527)
+      .onSuccess(httpServer -> System.out.println("server start at port "+httpServer.actualPort()))
+      .onFailure(Throwable::printStackTrace);
+  }
+}
