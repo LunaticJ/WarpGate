c18198a108e9b774bb9331b3063b32cdb952dafe
jason <ljc19940130@gmail.com>
2023-03-20 22:40:24 +0800

    init templar module, a test http server, and test launcher

---
+++ templar/src/main/java/com/lunaticj/wrapGate/templar/MainVerticle.java
@@ -0,0 +1,26 @@
+package com.lunaticj.wrapGate.templar;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Handler;
+import io.vertx.core.Promise;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.ext.web.Router;
+
+public class MainVerticle extends AbstractVerticle {
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    vertx.createHttpServer().requestHandler(req -> {
+      req.response()
+        .putHeader("content-type", "text/plain")
+        .end("Hello from Vert.x!");
+    }).listen(4399, http -> {
+      if (http.succeeded()) {
+        startPromise.complete();
+        System.out.println("HTTP server started on port 8888");
+      } else {
+        startPromise.fail(http.cause());
+      }
+    });
+  }
+}
