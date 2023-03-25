37863325412155861ff93b600ca1bf5f81b037f2
jason <ljc19940130@gmail.com>
2023-03-25 22:51:49 +0800

    Timeouthandler and responseTimeHandler test

--- templar/src/main/java/com/lunaticj/wrapGate/templar/MainVerticle.java
+++ templar/src/main/java/com/lunaticj/wrapGate/templar/MainVerticle.java
@@ -10,7 +10,10 @@ import io.vertx.core.impl.logging.LoggerFactory;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.ResponseTimeHandler;
+import io.vertx.ext.web.handler.TimeoutHandler;
 
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class MainVerticle extends AbstractVerticle {
@@ -18,42 +21,62 @@ public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) {
-    Router router = Router.router(vertx);
-    router.route().handler(ctx -> {
-      LOGGER.info("Hello! "+ctx.request().remoteAddress());
-      ctx.next();
-    });
-    router.route()
-      .handler(BodyHandler.create())
-      .blockingHandler(ctx -> {
-        HttpServerRequest request = ctx.request();
-        long block = 0;
-        int statusCode = 200;
-        JsonObject requestBody = JsonObject.of("code", 200, "msg", "quick api!");
-        try {
-          String blockStr = request.getParam("block", "");
-          String codeStr = request.getParam("code", "200");
-          String statusCodeStr = request.getParam("statusCode", "200");
-          if (!StringUtil.isNullOrEmpty(blockStr) && (block = Long.parseLong(blockStr)) > 0L) {
-            Thread.sleep(block);
-          }
-          if (!StringUtil.isNullOrEmpty(statusCodeStr)) {
-            statusCode = Integer.parseInt(statusCodeStr);
-          }
-          if (!StringUtil.isNullOrEmpty(codeStr)) {
-            requestBody.put("code", codeStr);
-          }
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
-        ctx.response().setStatusCode(statusCode).end(requestBody.toString());
-      });
+
     ConfigRetriever retriever = ConfigRetriever.create(vertx);
     retriever.getConfig()
-      .compose(config -> vertx.createHttpServer().requestHandler(router).listen(config.getInteger("server_port")))
-      .onSuccess(httpServer -> LOGGER.info("HTTP server started on port " + httpServer.actualPort()))
+      .compose(config -> {
+        Router router = Router.router(vertx);
+        this.sayHi(router);
+        router.route()
+          .handler(TimeoutHandler.create(30000))
+          .handler(ResponseTimeHandler.create())
+          .handler(BodyHandler.create())
+          .blockingHandler(ctx -> {
+            LOGGER.info("work on "+Thread.currentThread());
+            HttpServerRequest request = ctx.request();
+            JsonObject requestBody = config.getJsonObject("response");
+            Optional.ofNullable(request.getParam("code")).ifPresent(code -> requestBody.put("code", code));
+            this.block(request.getParam("block", ""));
+            ctx.response().setStatusCode(Optional.ofNullable(request.getParam("statusCode"))
+              .filter(statusCode -> !StringUtil.isNullOrEmpty(statusCode))
+              .map(statusCode -> Integer.getInteger(statusCode))
+              .orElse(200))
+              .send(requestBody.toString());
+            ctx.next();
+          }, false);
+        this.sayBye(router);
+        return vertx.createHttpServer().requestHandler(router).listen(config.getInteger("server_port"));
+      }).onSuccess(httpServer -> LOGGER.info("HTTP server started on port " + httpServer.actualPort()))
       .onFailure(throwable -> LOGGER.error(throwable.getMessage(), throwable));
 
 
   }
+
+  private void sayHi(Router router) {
+    router.route().handler(ctx -> {
+      LOGGER.info("Hello! "+ctx.request().remoteAddress());
+      ctx.put("startTimeStamp", System.currentTimeMillis());
+      ctx.next();
+    });
+  }
+
+  private void sayBye(Router router) {
+    router.route().handler(ctx -> {
+      Long startTimeStamp =ctx.get("startTimeStamp");
+      long used = System.currentTimeMillis()-startTimeStamp;
+      LOGGER.info("Bye! used "+used);
+    });
+  }
+
+  private void block(String blockStr) {
+    long block;
+    if (!StringUtil.isNullOrEmpty(blockStr) && (block = Long.parseLong(blockStr)) > 0L) {
+      try {
+        Thread.sleep(block);
+      } catch (InterruptedException e) {
+        LOGGER.error(e.getMessage(), e);
+      }
+    }
+  }
 }
+
