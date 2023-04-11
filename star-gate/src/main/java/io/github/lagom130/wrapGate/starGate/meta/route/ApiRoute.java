44740bc17c90b696ee6dab78400706bf15a99ab9
jason <ljc19940130@gmail.com>
2023-04-11 23:11:37 +0800

    add meta router 2

--- star-gate/src/main/java/io/github/lagom130/wrapGate/starGate/meta/route/ApiRoute.java
+++ star-gate/src/main/java/io/github/lagom130/wrapGate/starGate/meta/route/ApiRoute.java
@@ -2,36 +2,56 @@ package io.github.lagom130.wrapGate.starGate.meta.route;
 
 import io.vertx.core.Vertx;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
 
 public class ApiRoute {
-  protected static void addRoute(Vertx vertx, Router router) {
-    router.get("/meta/apis").handler(ctx -> {
-      //todo:
-      ctx.end();
-    });
-    router.get("/meta/apis/:id").handler(ctx -> {
-      String id = ctx.pathParam("id");
-      //todo:
-      ctx.end();
-    });
-    router.get("/meta/apis/:id/clients").handler(ctx -> {
-      String id = ctx.pathParam("id");
-      //todo:
-      ctx.end();
-    });
-    router.delete("/meta/apis/:id").handler(ctx -> {
-      String id = ctx.pathParam("id");
-      //todo:
-      ctx.end();
-    });
-    router.put("/meta/apis/:id").consumes("*/json").handler(BodyHandler.create()).handler(ctx -> {
-      //todo:
-      ctx.end();
-    });
-    router.post("/meta/apis").consumes("*/json").handler(BodyHandler.create()).handler(ctx -> {
-      //todo:
-      ctx.end();
-    });
+  private Vertx vertx;
+
+  public ApiRoute(Vertx vertx) {
+    this.vertx = vertx;
+  }
+
+  public void addRoute( Router router) {
+    router.get("/meta/apis").handler(this::getList);
+    router.get("/meta/apis/:id").handler(this::getOne);
+    router.get("/meta/apis/:id/clients").handler(this::getClients);
+    router.delete("/meta/apis/:id").handler(this::deleteOne);
+    router.put("/meta/apis/:id").consumes("*/json").handler(BodyHandler.create()).handler(this::updateOne);
+    router.post("/meta/apis").consumes("*/json").handler(BodyHandler.create()).handler(this::addOne);
+  }
+
+  public void getOne(RoutingContext ctx){
+    String id = ctx.pathParam("id");
+    //todo:
+    ctx.end();
+  }
+
+  public void getList(RoutingContext ctx){
+    //todo:
+    ctx.end();
+  }
+
+  public void getClients(RoutingContext ctx){
+    String id = ctx.pathParam("id");
+    //todo:
+    ctx.end();
+  }
+
+  public void addOne(RoutingContext ctx){
+    //todo:
+    ctx.end();
+  }
+
+  public void updateOne(RoutingContext ctx){
+    String id = ctx.pathParam("id");
+    //todo:
+    ctx.end();
+  }
+
+  public void deleteOne(RoutingContext ctx){
+    String id = ctx.pathParam("id");
+    //todo:
+    ctx.end();
   }
 }
--- star-gate/src/main/java/io/github/lagom130/wrapGate/starGate/meta/route/MetaRouteBuilder.java
+++ star-gate/src/main/java/io/github/lagom130/wrapGate/starGate/meta/route/MetaRouteBuilder.java
@@ -3,11 +3,10 @@ package io.github.lagom130.wrapGate.starGate.meta.route;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
-import io.vertx.ext.web.handler.BodyHandler;
 
 public class MetaRouteBuilder {
   public static Router build(Vertx vertx, Router router, JsonObject config) {
-    ApiRoute.addRoute(vertx, router);
+    new ApiRoute(vertx).addRoute(router);
     ClientRoute.addRoute(vertx, router);
     return router;
   }
