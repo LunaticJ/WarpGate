d5b6725183a6b95784f0c64c7c7bc09b2cf2f20a
jason <ljc19940130@gmail.com>
2023-04-25 23:59:00 +0800

    client entity init

---
+++ star-gate/src/main/java/io/github/lagom130/wrapGate/starGate/meta/client/ClientDO.java
@@ -0,0 +1,69 @@
+package io.github.lagom130.wrapGate.starGate.meta.client;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.OffsetDateTime;
+
+public class ClientDO {
+  private String id;
+  private String key;
+  private String secret;
+  @JsonProperty("tenant_id")
+  private String tenantId;
+  @JsonProperty("updated_time")
+  private OffsetDateTime updatedTime;
+  @JsonProperty("created_time")
+  private OffsetDateTime createdTime;
+
+  public ClientDO() {
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getKey() {
+    return key;
+  }
+
+  public void setKey(String key) {
+    this.key = key;
+  }
+
+  public String getSecret() {
+    return secret;
+  }
+
+  public void setSecret(String secret) {
+    this.secret = secret;
+  }
+
+  public String getTenantId() {
+    return tenantId;
+  }
+
+  public void setTenantId(String tenantId) {
+    this.tenantId = tenantId;
+  }
+
+  public OffsetDateTime getUpdatedTime() {
+    return updatedTime;
+  }
+
+  public void setUpdatedTime(OffsetDateTime updatedTime) {
+    this.updatedTime = updatedTime;
+  }
+
+  public OffsetDateTime getCreatedTime() {
+    return createdTime;
+  }
+
+  public void setCreatedTime(OffsetDateTime createdTime) {
+    this.createdTime = createdTime;
+  }
+}
